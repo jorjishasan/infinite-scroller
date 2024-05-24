552ebd8e8fbd93b704d1b898bb8d72d4d41ad7b2
jorjishasan <dev.jorjis@gmail.com>
2024-05-25 03:03:04 +0600

    feat: Implement lazy loading for images and infinite scrolling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
 
 function App() {
+  const [numArr, setNumArr] = useState(Array.from({ length: 20 }, (_, i) => i));
+
+  const fetchMoreData = () => {
+    setNumArr((prevNums) => [
+      ...prevNums,
+      ...Array.from({ length: 20 }, (_, i) => i + prevNums.length),
+    ]);
+  };
+
+  const handleScroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      fetchMoreData();
+    }
+  };
+
+  // Listen for scroll events
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="font-serif text-3xl text-center">
+      {numArr.map((num, index) => (
+        <div
+          key={index}
+          className="m-2 p-5 bg-white shadow-md rounded cursor-pointer hover:shadow-lg transition-shadow duration-300 ease-in-out"
         >
-          Learn React
-        </a>
-      </header>
+          {num}
+        </div>
+      ))}
     </div>
   );
 }
-
 export default App;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
